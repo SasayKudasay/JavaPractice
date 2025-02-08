8a2c9bae27f88c74e3fa19b583e8f0e55411e0a6
SasayKudasay <den.vutto@gmail.com>
2025-02-08 20:34:34 +0200

    Added task 5_2, Chain of responsibility

--- src/Task3/Task3_1/AbstractCommand.java
+++ src/Task3/Task3_1/AbstractCommand.java
@@ -3,6 +3,4 @@ package Task3.Task3_1;
 public abstract class AbstractCommand {
     abstract public void execute();
     abstract public void undo();
-
-
 }
--- src/Task5/Task5_2/Main.java
+++ src/Task5/Task5_2/Main.java
@@ -2,6 +2,14 @@ package Task5.Task5_2;
 
 public class Main {
     public static void main(String[] args) {
-
+        TransportInfo vehicle = new TransportInfo(true, false, 4);
+        DocumentCheckHandler documentCheck = new DocumentCheckHandler();
+        FineCheckHandler fineCheck = new FineCheckHandler();
+        EmissionCheckHandler emissionCheck = new EmissionCheckHandler();
+        RegistrationCheckHandler registration = new RegistrationCheckHandler();
+        documentCheck.addHandler(fineCheck);
+        fineCheck.addHandler(emissionCheck);
+        emissionCheck.addHandler(registration);
+        documentCheck.handle(vehicle);
     }
 }
